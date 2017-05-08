504fc9780d4110a001fb2fd185db3822dcf5b1ea
Iker <pedrosalonso@hotmail.com>
2017-05-08 09:37:57 +0200

    Added license

--- fileManager.py
+++ fileManager.py
@@ -1,3 +1,25 @@
+'''
+ ' fileManager.py
+ ' Author: Iker Pedrosa
+ ' 
+ ' License:
+ ' This file is part of orderedFileCopy.
+ ' 
+ ' orderedFileCopy is free software: you can redistribute it and/or modify
+ ' it under the terms of the GNU General Public License as published by
+ ' the Free Software Foundation, either version 3 of the License, or
+ ' (at your option) any later version.
+ ' 
+ ' orderedFileCopy is distributed in the hope that it will be useful,
+ ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ' GNU General Public License for more details.
+ ' 
+ ' You should have received a copy of the GNU General Public License
+ ' along with orderedFileCopy.  If not, see <http://www.gnu.org/licenses/>.
+ ' 
+'''
+
 #Imported modules
 import os
 from shutil import copyfile
